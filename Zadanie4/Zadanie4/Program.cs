3768f57526e3b927a44b11365a960557c45b9299
Mateusz Chorąży <mateusz.chorazy1@outlook.com>
2023-01-31 13:49:57 +0100

    Zadanie 4

---
+++ Zadanie4/Zadanie4/Program.cs
@@ -0,0 +1,18 @@
+﻿var name = "Ewa";
+bool isFemale = true;
+int age = 15;
+
+
+if(isFemale && age > 30)
+{
+    Console.WriteLine ("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa lat 33 "); 
+    
+}
+else if (isFemale && age < 18 )
+{
+    Console.WriteLine(" Niepełnoletni Mężczyzna ");
+}
