7ee4a838c6ed318af4fec65579ae258295b95e84
Mateusz Chorąży <mateusz.chorazy1@outlook.com>
2023-01-31 18:34:23 +0100

    for listy

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] grades = new int[365];
+List<string> dayOfweeks = new List<string>();
+dayOfweeks.Add("poniedziałek");
+dayOfweeks.Add("wtorek");
+dayOfweeks.Add("środa");
+dayOfweeks.Add("czwartek");
+dayOfweeks.Add("piątek");
+dayOfweeks.Add("sobota");
+dayOfweeks.Add("niedziela");
+
+
+foreach(var day in dayOfweeks) 
+{
+    Console.WriteLine(day);
+}
