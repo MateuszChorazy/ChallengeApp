d7acc85fcb2ad677ed646b2b49775ae23d549cec
Mateusz Chorąży <mateusz.chorazy1@outlook.com>
2023-02-27 20:43:25 +0100

    Testy dzień 9

---
+++ ChallengeApp/ChallengeApp.Tests/TypeTests.cs
@@ -0,0 +1,40 @@
+﻿namespace ChallengeApp.Tests
+{
+    public class TypeTests
+    {
+
+
+        [Test]
+        public void WhenTwoLoginIsSame()
+        {
+            var User1 = GetUser("Mateusz");
+            var User2 = GetUser("Mateusz");
+            Assert.AreEqual(User1.Login, User2.Login);
+
+        }
+        [Test]
+        public void WhenNumberIsSame()
+        {
+            int number1  = 2;
+            int number2  = 2;
+            Assert.AreEqual(number1, number2);
+
+
+        }
+        [Test]
+        public void TwoObjectsNotEqual()
+        {
+            string Employee1 = "Mateusz"; 
+            string Employee2 = "Adam"; 
+            Assert.AreNotEqual(Employee1, Employee2);
+
+
+        }
+
+        private User GetUser(string name)
+        {
+            return new User(name);
+        }
+    }
+}
+
